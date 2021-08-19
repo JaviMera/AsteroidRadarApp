f03676c8359f3a50214dfedbe1ccfcfb0d41c01b
JaviMera <javier_mera88@hotmail.com>
2021-08-19 09:10:01 -0500

    Refactored detail fragment to use a view model class to load the selected asteroid.

---
+++ app/src/main/java/com/udacity/asteroidradar/detail/DetailViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.udacity.asteroidradar.detail
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.asteroidradar.Asteroid
+import java.lang.IllegalArgumentException
+
+class DetailViewModelFactory(
+    private val asteroid: Asteroid,
+    private val application: Application
+    ) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(DetailViewModel::class.java)){
+            return DetailViewModel(asteroid, application) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class ${modelClass.canonicalName}")
+    }
+}
