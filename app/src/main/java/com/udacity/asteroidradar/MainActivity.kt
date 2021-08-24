52beaa383b335fd4275a8747c2d68060a819cf8e
JaviMera <javier_mera88@hotmail.com>
2021-08-24 17:33:52 -0500

    Begin adding worker logic to save asteroids to the database.

--- app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -2,6 +2,10 @@ package com.udacity.asteroidradar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.CoroutineWorker
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.udacity.asteroidradar.work.SaveAsteroidsWorker
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         if(BuildConfig.DEBUG){
             Timber.plant(Timber.DebugTree())
         }
+
+        val asteroidsWorkerRequest = OneTimeWorkRequestBuilder<SaveAsteroidsWorker>()
+            .build()
+
+        WorkManager
+            .getInstance()
+            .enqueue(asteroidsWorkerRequest)
     }
 }
---
+++ app/src/main/java/com/udacity/asteroidradar/work/SaveAsteroidsWorker.kt
@@ -0,0 +1,46 @@
+package com.udacity.asteroidradar.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.api.NasaApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import com.udacity.asteroidradar.database.AsteroidRadarDatabase
+import com.udacity.asteroidradar.database.toAsteroidEntities
+import com.udacity.asteroidradar.main.MainViewModel
+import org.json.JSONObject
+import retrofit2.await
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
+
+class SaveAsteroidsWorker(appContext: Context, workerParams: WorkerParameters) :
+    CoroutineWorker(appContext, workerParams){
+
+    override suspend fun doWork(): Result {
+        try{
+            val calendar = Calendar.getInstance()
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+            val calendarFuture = Calendar.getInstance()
+            calendarFuture.add(Calendar.DATE, 7)
+            val asteroidsRequest = NasaApi.asteroidsService.getAsteroids(
+                MainViewModel.API_KEY,
+                simpleDateFormat.format(calendar.time),
+                simpleDateFormat.format(calendarFuture.time)
+            )
+
+            val result = asteroidsRequest.await()
+            val asteroids = parseAsteroidsJsonResult(JSONObject(result))
+            val asteroidsDao = AsteroidRadarDatabase.getInstance(applicationContext).asteroidRadarDao
+            asteroidsDao.insert(asteroids.toAsteroidEntities())
+
+            Timber.i("Saved ${asteroids.size.toString()} to the database.")
+            return Result.success()
+        }catch (exception: Exception){
+
+            Timber.i("Unable to save asteroids from worker:\n${exception.message}")
+            return Result.failure()
+        }
+    }
+}
